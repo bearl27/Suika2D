b2121bf6fc4e22df45ba08cf69a3be1717e6e917
bearl <125728897+bearl27@users.noreply.github.com>
2024-06-01 00:00:08 +0900

    Player移動

--- Suika2D/Assets/cs/Player.cs
+++ Suika2D/Assets/cs/Player.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float maxX;
+    public float minX;
+    public float speed;
+
     void Update()
     {
-        
+        //ad movement
+        if(transform.position.x < maxX)
+        {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(1, 0, 0) * Time.deltaTime * speed;
+            }
+        }
+        if(transform.position.x > minX)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+            }
+        }
     }
 }
